01fb1bd811532f5924694908ef7b907b32d99cb1
Seung-Ki, Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2026-06-24 00:01:23 +0900

    스프라이트 활용

--- source/main.lua
+++ source/main.lua
@@ -1,24 +1,53 @@
+import "CoreLibs/sprites"
+
 -- Env.
 local pd = playdate
 local gfx = pd.graphics
 
 -- Player
-local playerPosX = 40
-local playerPosY = 120
+local playerStartPosX = 40
+local playerStartPosY = 120
 local playerSpeed = 3
-local playerSprite = gfx.image.new("images/capybara.png")
+local playerImage = gfx.image.new("images/capybara.png")
+local playerSprite = gfx.sprite.new(playerImage)
+
+-- Game State
+local gameState = "STOPPED"
 
 
-function pd.update()
-    gfx.clear() -- Screen Reset
 
-    local crankPosition = pd.getCrankPosition() -- Clockwise; 0 ~ 270
-
-    if crankPosition <= 90 or crankPosition >= 270 then
-        playerPosY = playerPosY - playerSpeed
-    else
-        playerPosY = playerPosY + playerSpeed
-    end
-
-    playerSprite:draw(playerPosX, playerPosY)
+-- Init
+local function init()
+    playerSprite:setCollideRect(4, 4, 56, 40) -- (posX, posY, width, height) 
+    playerSprite:moveTo(playerStartPosX, playerStartPosY)
+    playerSprite:add()
 end
+
+
+-- Update
+function pd.update()
+     gfx.sprite.update()
+
+    if gameState == "STOPPED" then
+        gfx.drawText("Press A to Start", 200, 40)
+
+        if pd.buttonJustPressed(pd.kButtonA) then
+            gameState = "ACTIVE"
+            playerSprite:moveTo(playerStartPosX, playerStartPosY)
+        end
+    elseif gameState == "ACTIVE" then
+        local crankPosition = pd.getCrankPosition() -- Clockwise; 0 ~ 270
+
+        if crankPosition <= 90 or crankPosition >= 270 then
+            playerSprite:moveBy(0, -playerSpeed) -- posX, posY
+        else
+            playerSprite:moveBy(0, playerSpeed) -- posX, posY 
+        end
+    end
+end
+
+
+
+
+
+init()
