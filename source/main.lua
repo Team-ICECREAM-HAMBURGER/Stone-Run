ec71dde51ae3046c2a519055bc5f712216c11476
Seung-Ki Kim <62886544+Seung-Ki-Kim@users.noreply.github.com>
2026-06-29 23:55:13 +0900

    코드 정리

--- source/main.lua
+++ source/main.lua
@@ -4,68 +4,78 @@ import "CoreLibs/sprites"
 -- Env.
 local pd = playdate
 local gfx = pd.graphics
+TAG_OBSTACLE = 1
 
 -- Player
 local playerStartPosX = 40
 local playerStartPosY = 120
-local playerSpeed = 3
 local playerImage = gfx.image.new("images/car.png")
 local playerSprite = gfx.sprite.new(playerImage)
+local playerSpeed = 3
 
 -- Obstacle
 local obstacleStartPosX = 450
 local obstacleStartPosY = 240
 local obstacleImage = gfx.image.new("images/rock.png")
 local obstacleSprite = gfx.sprite.new(obstacleImage)
-local obstacleSpeed = 5
+obstacleSprite:setTag(TAG_OBSTACLE)
 
 -- Road
-local roadStartPosX = 200
+local roadStartPosXs = {200, 644}
 local roadStartPosY = 120
+local roadStackPos = 444
 local roadImage = gfx.image.new("images/road.png")
 local roadSprites = {gfx.sprite.new(roadImage), gfx.sprite.new(roadImage)}
-local roadSpeed = 5
+for i = 1, #roadSprites do
+    roadSprites[i]:setTag(TAG_OBSTACLE)
+end
 
--- GuardRail
+-- Upper GuardRail
 local guardRailStartPosX = 200
-local guardRailStartPosY = 0
-local guardRailImage = gfx.image.new("images/guard.png")
-local guardRailSprites = {gfx.sprite.new(guardRailImage), gfx.sprite.new(guardRailImage)}
+local guardRailStartPosYs = {10, 230}
+local upperGuardRailImage = gfx.image.new("images/guard_u.png")
+local lowerGuardRailImage = gfx.image.new("images/guard_l.png")
+local guardRailSprites = {gfx.sprite.new(upperGuardRailImage), gfx.sprite.new(lowerGuardRailImage)}
+for i = 1, #guardRailSprites do
+    guardRailSprites[i]:setTag(TAG_OBSTACLE)
+end
 
 -- Game State
 local gameState = "STOPPED"
 local gameScore = 0
+local gameSpeed = 5
 
 -- Game UI
 local gameScoreUIText = "Score: 0"
+local gameUIPanelImage = gfx.image.new("images/panel.png")
 
 
 
 -- Init
 local function init()
-    roadSprites[1]:moveTo(roadStartPosX, roadStartPosY)
-    roadSprites[1]:add()
-    roadSprites[2]:moveTo(roadStartPosX+444, roadStartPosY)
-    roadSprites[2]:add()
+    for i = 1, 2, 1 do
+        roadSprites[i]:moveTo(roadStartPosXs[i], roadStartPosY)
+        roadSprites[i]:add()
+        roadSprites[i]:setZIndex(0)
 
-    guardRailSprites[1]:moveTo(guardRailStartPosX, guardRailStartPosY+10)
-    guardRailSprites[1]:add()
-    guardRailSprites[1].collisionResponse = gfx.sprite.kCollisionTypeOverlap
-    guardRailSprites[1]:setCollideRect(0, 0, 400, 20)
+        guardRailSprites[i]:moveTo(guardRailStartPosX, guardRailStartPosYs[i])
+        guardRailSprites[i]:add()
+        guardRailSprites[i].collisionResponse = gfx.sprite.kCollisionTypeOverlap
+        guardRailSprites[i]:setCollideRect(0, 0, 400, 20)
+        guardRailSprites[i]:setZIndex(2)
+    end
 
-    playerSprite:setCollideRect(0, 0, 28, 26) -- (posX, posY, width, height) 
     playerSprite:moveTo(playerStartPosX, playerStartPosY)
     playerSprite:add()
+    playerSprite.collisionResponse = gfx.sprite.kCollisionTypeOverlap
+    playerSprite:setCollideRect(0, 0, 28, 26)
+    playerSprite:setZIndex(1)
 
-    guardRailSprites[2]:moveTo(guardRailStartPosX, guardRailStartPosY+230)
-    guardRailSprites[2]:add()
-    guardRailSprites[2].collisionResponse = gfx.sprite.kCollisionTypeOverlap
-    guardRailSprites[2]:setCollideRect(0, 20, 400, 20)
-
-    obstacleSprite.collisionResponse = gfx.sprite.kCollisionTypeOverlap
-    obstacleSprite:setCollideRect(0, 0, 48, 48)
     obstacleSprite:moveTo(obstacleStartPosX, obstacleStartPosY)
     obstacleSprite:add()
+    obstacleSprite.collisionResponse = gfx.sprite.kCollisionTypeOverlap
+    obstacleSprite:setCollideRect(0, 0, 48, 48)
+    obstacleSprite:setZIndex(2)
 end
 
 
@@ -80,60 +90,72 @@ end
 function pd.update()
     gfx.sprite.update()
 
-    if gameState == "STOPPED" then  -- Game STOPPED
+    -- Game STOP
+    if gameState == "STOPPED" then
+        gameUIPanelImage:draw(100, 20)
         gfx.drawTextAligned("Press A to Start", 200, 40, kTextAlignment.center)
         gfx.drawTextAligned(gameScoreUIText, 200, 70, kTextAlignment.center)
 
-        if pd.buttonJustPressed(pd.kButtonA) then  -- Game Start
+        -- Game START
+        if pd.buttonJustPressed(pd.kButtonA) then
             gameState = "ACTIVE"
+
             updateScore(-gameScore)
-            obstacleSpeed = 5
+            gameSpeed = 5
 
             playerSprite:moveTo(playerStartPosX, playerStartPosY)
             obstacleSprite:moveTo(obstacleStartPosX, math.random(40, 200))
 
-            roadSprites[1]:moveTo(roadStartPosX, roadStartPosY)
-            roadSprites[2]:moveTo(roadStartPosX+444, roadStartPosY)
+            for i = 1, 2, 1 do
+                roadSprites[i]:moveTo(roadStartPosXs[i], roadStartPosY)
+            end
         end
-    elseif gameState == "ACTIVE" then  -- Game ACTIVATED
+
+    -- Game UPDATE
+    elseif gameState == "ACTIVE" then
         local crankPosition = pd.getCrankPosition() -- Clockwise; 0 ~ 270
+        local playerPosYoffset = (crankPosition <= 90 or crankPosition >= 270) and -playerSpeed or playerSpeed
+        local _, _, playerCollisions, _ = playerSprite:moveWithCollisions(playerSprite.x, playerSprite.y + playerPosYoffset)
 
-        if crankPosition <= 90 or crankPosition >= 270 then
-            playerSprite:moveBy(0, -playerSpeed) -- posX, posY
-        else
-            playerSprite:moveBy(0, playerSpeed) -- posX, posY 
-        end
+        -- obstacle movement
+        obstacleSprite:moveWithCollisions(obstacleSprite.x - gameSpeed, obstacleSprite.y)
 
-        local actualX, actualY, collisions, obstacleLength = obstacleSprite:moveWithCollisions(obstacleSprite.x - obstacleSpeed, obstacleSprite.y)
-
+        -- obstacle reset
         if obstacleSprite.x < -20 then
-            obstacleSprite:moveTo(obstacleStartPosX, math.random(40, 200)) -- obstacle reset
+            obstacleSprite:moveTo(obstacleStartPosX, math.random(40, 200))
 
             updateScore(1)
-            obstacleSpeed = obstacleSpeed + 0.5
-            roadSpeed = roadSpeed + 0.5
+            gameSpeed = gameSpeed + 0.5
         end
 
-        local playerCollider = playerSprite:overlappingSprites()
-
-        roadSprites[1]:moveBy(-roadSpeed, 0)
-        roadSprites[2]:moveBy(-roadSpeed, 0)
-
-        if roadSprites[1].x < -200 then
-            roadSprites[1]:moveTo(roadSprites[2].x+444, roadStartPosY)
-        end
-        if roadSprites[2].x < -200 then
-            roadSprites[2]:moveTo(roadSprites[1].x+444, roadStartPosY)
+        -- road movement
+        for i = 1, 2, 1 do
+            roadSprites[i]:moveBy(-gameSpeed, 0)
         end
 
-        -- gfx.drawTextAligned(gameScoreUIText, 390, 10, kTextAlignment.right)
+        -- road rotation
+        for i = 1, 2 do
+            if roadSprites[i].x < -200 then
+                local other = (3 - i)
+                roadSprites[i]:moveTo(roadSprites[other].x + roadStackPos, roadStartPosY)
+            end
+        end
 
-        -- Gameover; length of collisions array; D = y - 30
-        if #playerCollider > 0 then
-            gameState = "STOPPED"
+        -- Gameover;
+        if playerCollisions and #playerCollisions > 0 then
+            for i = 1, #playerCollisions do
+                local collision = playerCollisions[i]
+                local otherTag = collision.other:getTag()
+
+                if otherTag == TAG_OBSTACLE then
+                    gameState = "STOPPED"
+                end
+            end
         end
     end
 end
+
+
 
 
 
